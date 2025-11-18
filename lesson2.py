bca4faf133863f65539b6c5d6661d29761d88691
sky <159075384+skymeiling@users.noreply.github.com>
2025-11-18 05:37:59 +0000

    lesson2

--- lesson2.py
+++ lesson2.py
@@ -1,1 +1,16 @@
+# First, import the randint function from the random module
 
+# Then, generate a random number between 1 and 10
+
+# Next, prompt the user for a number between 1 and 10
+
+# After, compare the user's number with the random number
+
+# Finally, if the two numbers are the same, tell them they win the game. But if they are different, tell them they lost the game
+
+# EXTENSION (just edit your code above!)
+
+# Change the random number range from 1-10 to 1-100
+# Create a while loop that runs until the user guesses the random number
+# Inside the while loop, compare the user's guess with the random number — if the user guessed too high or too low, tell them that and prompt them for another number
+# Exit the while loop once they guess correctly, and be sure to tell the user they won!
